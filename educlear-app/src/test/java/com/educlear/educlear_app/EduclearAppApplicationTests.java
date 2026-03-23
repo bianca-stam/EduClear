52856444490b1af4b3bcb5ed02da4329ab49e6fb
bianca-stam <bianca.stefania.amariutei@gmail.com>
2026-03-23 16:19:26 +0000

    Inicializados el back y el front

---
+++ educlear-app/src/test/java/com/educlear/educlear_app/EduclearAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educlear.educlear_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduclearAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
