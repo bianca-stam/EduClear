ef540672d41f106ead17d1eede4bc7b445e07884
AdrianQH18 <152377366+AdrianQH18@users.noreply.github.com>
2026-04-12 23:21:29 +0200

    Corrigiendo el microServicio

--- educlear-service-Backend/usuario-service/src/main/java/org/example/serviciousuario/dto/UsuarioDTO.java
+++ educlear-service-Backend/usuario-service/src/main/java/org/example/serviciousuario/dto/UsuarioDTO.java
@@ -1,9 +1,14 @@
 package org.example.serviciousuario.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.example.serviciousuario.model.Rol;
 
-@Data
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UsuarioDTO {
     private Integer id;
     private String username;
