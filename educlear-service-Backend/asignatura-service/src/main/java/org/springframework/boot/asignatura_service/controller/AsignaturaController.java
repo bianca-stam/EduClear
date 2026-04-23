ae546ca0de53bf02ab335e4f08cfc3c06a0e610c
bianca-stam <bianca.stefania.amariutei@gmail.com>
2026-04-23 21:46:53 +0100

    Añadido endpoint /api/asignatura para POST y GET de todo y GET por id

---
+++ educlear-service-Backend/asignatura-service/src/main/java/org/springframework/boot/asignatura_service/controller/AsignaturaController.java
@@ -0,0 +1,40 @@
+package org.springframework.boot.asignatura_service.controller;
+
+import org.springframework.boot.asignatura_service.dto.AsignaturaDTO;
+import org.springframework.boot.asignatura_service.service.AsignaturaService;
+import org.springframework.boot.asignatura_service.model.Asignatura;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/asignaturas")
+@CrossOrigin(origins = "*")
+public class AsignaturaController {
+
+    private AsignaturaService asignaturaService;
+
+    public AsignaturaController(AsignaturaService asignaturaService) {
+        this.asignaturaService = asignaturaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AsignaturaDTO>> getAll() {
+        return ResponseEntity.ok(asignaturaService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AsignaturaDTO> getById(@PathVariable Integer id) {
+        AsignaturaDTO asignatura = asignaturaService.findById(id);
+        return asignatura != null ? ResponseEntity.ok(asignatura)
+                : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<AsignaturaDTO> create(@RequestBody Asignatura asignatura) {
+        return new ResponseEntity<>(asignaturaService.save(asignatura), HttpStatus.CREATED);
+    }
+
+}
---
+++ educlear-service-Backend/asignatura-service/src/main/java/org/springframework/boot/asignatura_service/dto/AsignaturaDTO.java
@@ -0,0 +1,15 @@
+package org.springframework.boot.asignatura_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AsignaturaDTO {
+    private Integer id;
+    private String nombre;
+    private Integer cursoId;
+    private Integer profesorId;
+}
