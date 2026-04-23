cab35d9d5cf8f364b61817e57ea3b5bce6ae980b
AdrianQH18 <adrian.qh18@gmail.com>
2026-04-23 02:27:10 +0200

    token funcionable

--- educlear-service-Backend/usuario-service/src/main/java/org/example/serviciousuario/tokenjwt/JwtUtil.java
+++ educlear-service-Backend/usuario-service/src/main/java/org/example/serviciousuario/tokenjwt/JwtUtil.java
@@ -4,24 +4,26 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
 public class JwtUtil {
 
-    private static final String SECRET = "clave-secreta";
-    private static final long EXP = 1000 * 60 * 60;
 
-    public static String generarToken(Integer id, String username, String rol) {
+    private static final SecretKey KEY =
+            Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public static String generarToken(Integer id, String email, String rol) {
         return Jwts.builder()
-                .setSubject(username)
+                .setSubject(email)
                 .claim("id", id)
                 .claim("rol", rol)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXP))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(KEY)
                 .compact();
-
     }
+
 
 }
