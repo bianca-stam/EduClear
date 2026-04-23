11eac132ce497e800d28e1177d35359b13b196cf
bianca-stam <bianca.stefania.amariutei@gmail.com>
2026-04-23 13:14:28 +0100

    Creado microservicio asignatura-service

---
+++ educlear-service-Backend/asignatura-service/src/main/java/org/springframework/boot/asignatura_service/AsignaturaServiceApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.asignatura_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsignaturaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsignaturaServiceApplication.class, args);
+	}
+
+}
---
+++ educlear-service-Backend/asignatura-service/src/test/java/org/springframework/boot/asignatura_service/AsignaturaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.asignatura_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsignaturaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
