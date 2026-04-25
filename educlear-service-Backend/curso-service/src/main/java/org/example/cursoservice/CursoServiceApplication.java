06394e56974879fafdd238db1287530f5fe10e9c
AdrianQH18 <adrian.qh18@gmail.com>
2026-04-25 22:42:58 +0200

    modificaciones en el servicio curso

--- educlear-service-Backend/curso-service/src/main/java/org/example/cursoservice/CursoServiceApplication.java
+++ educlear-service-Backend/curso-service/src/main/java/org/example/cursoservice/CursoServiceApplication.java
@@ -3,7 +3,7 @@ package org.example.cursoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "org.example.cursoservice")
+@SpringBootApplication
 public class CursoServiceApplication {
 
     public static void main(String[] args) {
--- educlear-service-Backend/curso-service/src/main/java/org/example/cursoservice/service/CursoService.java
+++ educlear-service-Backend/curso-service/src/main/java/org/example/cursoservice/service/CursoService.java
@@ -2,14 +2,16 @@ package org.example.cursoservice.service;
 
 import org.example.cursoservice.dto.CreateCursoDto;
 import org.example.cursoservice.dto.CursoDto;
-import org.example.cursoservice.model.Curso;
 
 import java.util.List;
 
 public interface CursoService {
     List<CursoDto> findAll();
+
     CursoDto findById(Integer id);
+
     CursoDto save(CreateCursoDto curso);
+
     void delete(Integer id);
 
     // Cursos que imparte un profesor
